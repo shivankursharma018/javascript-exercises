4c20a8720ef5b0603dfa22225307f68b6266203f
shivankursharma018 <shivankursharma018@gmail.com>
2024-06-01 19:01:19 +0530

    02 repeatstring done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) return "ERROR";
+    let myString = "";
+    for (let i=1; i<=num; i++) {
+        myString += word
+    }
+    return myString;
 };
 
 // Do not edit below this line
