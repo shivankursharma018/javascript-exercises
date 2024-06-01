ef7b84bc37a783171681b30f34eba72818b49e32
shivankursharma018 <shivankursharma018@gmail.com>
2024-06-01 21:22:49 +0530

    04 remove from array done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ele) {
+    eleIndex = arr.indexOf(ele);
+    arr.splice(eleIndex, 1);
+    return arr;
 };
 
 // Do not edit below this line
