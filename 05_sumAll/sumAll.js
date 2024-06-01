4869910841598f590f29ccf2598f7fd9fca6b27d
shivankursharma018 <shivankursharma018@gmail.com>
2024-06-01 23:20:04 +0530

    05 sum all initial test done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    let sum = 0;
 
+    if (n1 < n2) {
+        for (let i=n1; i<=n2; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else if (n1 > n2) {
+        for (let j=n2; j<=n1; j++) {
+            sum += j;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
