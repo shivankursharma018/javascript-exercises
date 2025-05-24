aa75cb0dc3483408bcf70c7415623a8ce161339f
shivankursharma018 <shivankursharma018@gmail.com>
2025-05-24 12:12:38 +0530

    11/11 tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    fib = [0, 1];
 
+    if (num === 1)
+        return 1;
+    else if (num === 2)
+        return fib[0] + fib[1];
+    else if (num > 1) {
+        for (let i=2; i<=num; i++)
+            fib.push(fib[i-1] + fib[i-2])
+    } else if (num < 0)
+        return "OOPS";
+    return fib[num];
 };
 
 // Do not edit below this line
