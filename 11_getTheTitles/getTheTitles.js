d64259064bec2e3f589136abf4f749b30ffcadc9
shivankursharma018 <shivankursharma018@gmail.com>
2025-05-24 12:33:47 +0530

    1/1 tests passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
 
+const getTheTitles = function(books) {
+    let titles = [];
+
+    for (let i=0; i < books.length; i++)
+        titles.push(books[i].title);
+    return titles;
 };
-
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
